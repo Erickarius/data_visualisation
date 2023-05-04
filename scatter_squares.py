eb525a3494db642537e997ac25704f2e02151eb0
Eryk <erykkurkowski.praca@gmail.com>
2023-05-04 11:25:33 +0200

    automatic calculation of data, definition of custom colors, use of a color map and automatic saving of the chart.

--- scatter_squares.py
+++ scatter_squares.py
@@ -2,11 +2,11 @@ import matplotlib.pyplot as plt
 
 plt.style.use('seaborn')
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
 
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=100)
+ax.scatter(x_values, y_values,c=y_values, cmap=plt.cm.Blues, s=10)
 
 #Zdefiniowanie tytułu wykresu i etykiet osi.
 ax.set_title("Kwadraty liczb", fontsize=24)
@@ -16,4 +16,7 @@ ax.set_ylabel("Kwadrat wartości", fontsize=14)
 #Zdefiniowanie wielkości etykiet.
 ax.tick_params(axis='both', which='major', labelsize=14)
 
-plt.show()
+#Zdefiniowanie zakresu dla każdej osci.
+ax.axis([0, 1100, 0, 1100000])
+
+plt.savefig('squares_plot.png', bbox_inches='tight')
