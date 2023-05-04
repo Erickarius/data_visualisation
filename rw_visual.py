1224070bf6739d78185cf5c900f9ddc6f64ca5fa
Eryk <erykkurkowski.praca@gmail.com>
2023-05-04 12:38:01 +0200

    Giving style to data generated by random wandering, coloring points, coloring start and end points

--- rw_visual.py
+++ rw_visual.py
@@ -5,14 +5,21 @@ from random_walk import RandomWalk
 #Przygotowaniedanych błądzenia losowego, dopóki program pozostaje aktywny
 
 while True:
-	#Przygotowanie danych błądzenia losowego.
+	#Przygotowanie danych błądzenia losowego i wyświetlanie punktów.
 	rw = RandomWalk()
 	rw.fill_walk()
 
 	#Wyświetlenie punktów błądzenia losowego.
 	plt.style.use('classic')
 	fig, ax = plt.subplots()
-	ax.scatter(rw.x_values, rw.y_values, s=15)
+	point_numbers = range(rw.num_points)
+
+	ax.scatter(rw.x_values, rw.y_values, c=point_numbers, 
+		cmap=plt.cm.Blues, edgecolor='none', s=15)
+	ax.scatter(0, 0, c="green", edgecolor='none', s=100)
+	ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolor='none',
+		s=100)
+
 	plt.show()
 	keep_running = input("Utworzyć kolejne błądzenie losowe? (t/n): ")
 	if keep_running == 'n':
