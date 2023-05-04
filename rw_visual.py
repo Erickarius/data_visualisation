90ddedf1b22a76b555f9f7366247db1d9605203c
Eryk <erykkurkowski.praca@gmail.com>
2023-05-04 12:44:09 +0200

    Hiding axes and adding points to the chart

--- rw_visual.py
+++ rw_visual.py
@@ -6,7 +6,7 @@ from random_walk import RandomWalk
 
 while True:
 	#Przygotowanie danych błądzenia losowego i wyświetlanie punktów.
-	rw = RandomWalk()
+	rw = RandomWalk(50_000)
 	rw.fill_walk()
 
 	#Wyświetlenie punktów błądzenia losowego.
@@ -15,10 +15,14 @@ while True:
 	point_numbers = range(rw.num_points)
 
 	ax.scatter(rw.x_values, rw.y_values, c=point_numbers, 
-		cmap=plt.cm.Blues, edgecolor='none', s=15)
+		cmap=plt.cm.Blues, edgecolor='none', s=1)
 	ax.scatter(0, 0, c="green", edgecolor='none', s=100)
 	ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolor='none',
 		s=100)
+
+	#Ukrycie osi.
+	ax.get_xaxis().set_visible(False)
+	ax.get_yaxis().set_visible(False)
 
 	plt.show()
 	keep_running = input("Utworzyć kolejne błądzenie losowe? (t/n): ")
