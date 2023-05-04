9c16313d475754d570261ee314cb247f58f445dd
Eryk <erykkurkowski.praca@gmail.com>
2023-05-04 12:29:20 +0200

    Generate multiple random strays

--- rw_visual.py
+++ rw_visual.py
@@ -2,15 +2,18 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-#Przygotowaniedanych błądzenia losowego i wyświetlenie punktów.
+#Przygotowaniedanych błądzenia losowego, dopóki program pozostaje aktywny
 
-rw = RandomWalk()
-rw.fill_walk()
+while True:
+	#Przygotowanie danych błądzenia losowego.
+	rw = RandomWalk()
+	rw.fill_walk()
 
-#Wyświetlenie punktów błądzenia losowego.
-
-plt.style.use('classic')
-fig, ax = plt.subplots()
-
-ax.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+	#Wyświetlenie punktów błądzenia losowego.
+	plt.style.use('classic')
+	fig, ax = plt.subplots()
+	ax.scatter(rw.x_values, rw.y_values, s=15)
+	plt.show()
+	keep_running = input("Utworzyć kolejne błądzenie losowe? (t/n): ")
+	if keep_running == 'n':
+		break
