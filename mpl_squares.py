0fb0785da2bd7dbcc226de99677b5817b917a256
Eryk <erykkurkowski.praca@gmail.com>
2023-05-04 10:46:28 +0200

    Generate a simple chart, change the label and thickness of the chart, and improve the chart

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,7 +1,16 @@
 import matplotlib.pyplot as plt
 
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(input_values, squares, linewidth=3)
+
+#Zdefiniowanie tytułu wykresu i etykiet osi
+ax.set_title("Kwadraty liczb", fontsize=24)
+ax.set_xlabel("Wartość", fontsize=14)
+ax.set_ylabel("Kwadraty wartości", fontsize=14)
+
+#Zdefiniowanie wielkości etykiet.
+ax.tick_params(axis='both', labelsize=14)
 
 plt.show()
