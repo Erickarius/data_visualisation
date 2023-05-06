24b732ee224c2491a278a99a7ddc854c95a15d63
Eryk <erykkurkowski.praca@gmail.com>
2023-05-06 19:09:30 +0200

    D8 dice throw

--- dice_visual_D8_size.py
+++ dice_visual_D8_size.py
@@ -3,13 +3,13 @@ from plotly import offline
 
 from die import Die
 
-#Utworzoenie dwóch kości typu D6 i D10
-die_1 = Die()
-die_2 = Die(10)
+#Utworzoenie dwóch kości typu D8
+die_1 = Die(8)
+die_2 = Die(8)
 
 #Wykonanie pewnej liczby rzutów i umieszczenie wyników na liście.
 results = []
-for roll_num in range(50_000):
+for roll_num in range(10_000_000):
 	result = die_1.roll() + die_2.roll()
 	results.append(result)
 
@@ -27,6 +27,6 @@ data = [Bar(x=x_values, y=frequencies)]
 
 x_axis_config = {'title': 'Wynik', 'dtick': 1}
 y_axis_config = {'title': "Częstotliwość występowania wartości"}
-my_layout = Layout(title='Wynik rzucania kośćmi D6 i D10 pięćdziesiąt tysięcy razy',
+my_layout = Layout(title='Wynik rzucania dwoma kośćmi D8 dzięsięć milionów razy',
 	xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data': data, 'layout': my_layout}, filename='d6_d10.html')
+offline.plot({'data': data, 'layout': my_layout}, filename='d8_d8.html')
