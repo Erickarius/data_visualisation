bf537549ae54ace99f4f56435e84d323c874890a
Eryk <erykkurkowski.praca@gmail.com>
2023-05-06 13:58:44 +0200

    analysis of the results

--- die_visual.py
+++ die_visual.py
@@ -5,8 +5,14 @@ die = Die()
 
 #Wykonanie pewnej liczby rzutów i umieszczenie wyników na liście.
 results = []
-for roll_num in range(100):
+for roll_num in range(1000):
 	result = die.roll()
 	results.append(result)
 
-print(results)
+#Analiza wyników:
+frequencies = []
+for value in range(1, die.num_sides+1):
+	frequency = results.count(value)
+	frequencies.append(frequency)
+
+print(frequencies)
