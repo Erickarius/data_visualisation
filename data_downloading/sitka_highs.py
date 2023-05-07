55af709c8b864ca730ab318a678f20478d1117a2
Eryk <erykkurkowski.praca@gmail.com>
2023-05-07 14:05:48 +0200

    Displaying the second series of data

--- data_downloading/sitka_highs.py
+++ data_downloading/sitka_highs.py
@@ -9,20 +9,23 @@ with open(filename) as f:
 	header_row = next(reader)
 
 	#Pobieranie dat i najwyższych temperatur z pliku
-	dates, highs = [], []
+	dates, highs, lows = [], [], []
 	for row in reader:
 		current_date = datetime.strptime(row[2], "%Y-%m-%d")
 		high = int(row[5])
+		low = int(row[6])
 		dates.append(current_date)
 		highs.append(high)
+		lows.append(low)
 
 #Dane wykresu.
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
 ax.plot(dates, highs, c='red')
+ax.plot(dates, lows, c='blue')
 
 #Formatowanie wykresu.
-ax.set_title("Najwyższa temeperatura dnia - 2018", fontsize=24)
+ax.set_title("Najwyższa i najniższa temeperatura dnia - 2018", fontsize=24)
 ax.set_xlabel('', fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel("Temperatura (F)", fontsize=16)
