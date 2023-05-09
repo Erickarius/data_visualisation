a7a11b52bfc9c0545cb4799ce2d4816bcabbd12a
Eryk <erykkurkowski.praca@gmail.com>
2023-05-09 18:16:43 +0200

    Creation of a list of earthquakes

--- mapping_global_data_sets/eq_explore_data.py
+++ mapping_global_data_sets/eq_explore_data.py
@@ -4,6 +4,6 @@ import json
 filename = 'data/eq_data_1_day_m1.json'
 with open(filename) as f:
 	all_eq_data = json.load(f)
-	readalbe_file = 'data/readable_eq_data.json'
-	with open(readalbe_file, 'w') as f:
-		json.dump(all_eq_data, f, indent=4)
+
+all_eq_dicts = all_eq_data['features']
+print(len(all_eq_dicts))
