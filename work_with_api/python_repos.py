8c279ebfa47741ddc942f1f603da7655200ce728
Eryk <erykkurkowski.praca@gmail.com>
2023-05-12 14:01:26 +0200

    Works with answer dictionary

--- work_with_api/python_repos.py
+++ work_with_api/python_repos.py
@@ -7,6 +7,14 @@ r = requests.get(url, headers=headers)
 print(f"Kod stanu: {r.status_code}")
 #Umieszczenie odpowiedzi API w zmiennej.
 response_dict = r.json()
+print(f"Całkowita liczba repozytoriów: {response_dict['total_count']}")
 
-#Przetworzenie wyników
-print(response_dict.keys())
+#Przetworzenie informacji o repozytoriach.
+repo_dicts = response_dict['items']
+print(f"Liczba zwróconych repozytoriów: {len(repo_dicts)}")
+
+#Przeanalizowanie pierwszego repozytorium.
+repo_dict = repo_dicts[0]
+print(f"\nKlucze: {len(repo_dict)}")
+for key in sorted(repo_dict.keys()):
+	print(key)
